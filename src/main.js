86f3e58d30eae0fd60fd9f21210ba38ceb056be5
李长伟 <568084968@qq.com>
2020-04-20 22:07:10 +0800

    加载了element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载element组件库
+import ElementUI from 'element-ui'
+// 加载element组件库样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 加载全局样式
+import './styles/index.less'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
