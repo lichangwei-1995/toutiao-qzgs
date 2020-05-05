925e6f1c6683e028d04dc28be9704b37edad2cd4
李长伟 <568084968@qq.com>
2020-05-05 22:59:05 +0800

    更新

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -41,7 +41,12 @@
             :src="user.photo"
           >
           </el-avatar>
-          <p>点击修改头像</p>
+          <p @click="$refs.file.click()">点击修改头像</p>
+          <input
+            type="file"
+            ref="file"
+            hidden
+          >
         </el-col>
       </el-row>
     </el-card>
@@ -70,9 +75,7 @@ export default {
         resource: '',
         desc: ''
       },
-      user: {},
-      // fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
-      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+      user: {}
     }
   },
   computed: {},
